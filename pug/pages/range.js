c68fad3638bc7c5a0e2f0acc5c056528cfec7713
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-10-26 20:06:41 +0300

    refactor: second ui-kit page

--- pug/pages/range.js
+++ pug/pages/range.js
@@ -5,12 +5,12 @@ $(".range__range").slider({
     range: true,
     animate: "fast",
     slide : function(event, ui) {    
-        $(".range__result-text").text( `от ${ui.values[ 0 ]}₽ до ${ui.values[ 1 ]}₽` ); 
+        $(".range__result-text").text( `${ui.values[ 0 ]}₽ - ${ui.values[ 1 ]}₽` ); 
     }    
 });
 let result1= $(".range__range").slider("values", 0);
 let result2= $(".range__range").slider("values", 1);
 $(".range__thumb-left").val(result1);
 $(".range__thumb-right").val(result2);
-$( ".range__result-text" ).text(`от ${result1}₽ до ${result2}₽`);
+$( ".range__result-text" ).text(`${result1}₽ - ${result2}₽`);
 $("#range__result").val($(".range__range").slider("value"));
