7001bf4dcf79dfd10f742081027f71c43277f29b
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-11-09 10:16:09 +0500

    feat: card layout and style

---
+++ pug/components/inputs/date_drop_numb.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    $('.input__drop-text').datepicker({
+        toggleSelected: false, // возможость выбрать одну дату
+        prevHtml: `<i class = 'material-icons md-24 '> arrow_back </i>`,
+        nextHtml: `<i class = 'material-icons md-24 '> arrow_forward </i>`,
+        navTitles: {
+            days: 'MM yyyy',
+          },
+        //   clearButton: true,
+    })
+
+    $('.input__drop-numb').datepicker({
+        toggleSelected: false, // возможость выбрать одну дату
+        prevHtml: `<i class = 'material-icons md-24 '> arrow_back </i>`,
+        nextHtml: `<i class = 'material-icons md-24 '> arrow_forward </i>`,
+        navTitles: {
+            days: 'MM yyyy',
+          },
+    })
+    
+    $('.input__drop-filter').datepicker({
+        range: true, //выбор периода
+        toggleSelected: false, // возможость выбрать одну дату
+        prevHtml: `<i class = 'material-icons md-24 '> arrow_back </i>`,
+        nextHtml: `<i class = 'material-icons md-24 '> arrow_forward </i>`,
+        dateFormat: 'dd M',
+        multipleDatesSeparator: " - ",
+    })
+}
