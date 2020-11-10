63b93d4c91407c5fcb4aa6ae45fbb9196245a43a
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2020-11-10 20:48:56 +0500

    feat: dropdawn guests js

---
+++ pug/components/inputs/drop_down_guests.js
@@ -0,0 +1,83 @@
+window.onload = () => {
+    const clearBtn = $(`[data-action = 'clear']`);
+    const setResultBtn = $(`[data-action = 'set-result']`);
+    
+    const disabledButton = () => {
+        document.querySelectorAll('.quantity').forEach((el) => {
+            if (el.innerText == '0'){
+                el.previousElementSibling.classList.add('disabled');
+            } else {
+                el.previousElementSibling.classList.remove('disabled');
+            }
+        });
+    }
+
+    const onMinus = (e) => {
+        let number = e.target.nextElementSibling.innerText;
+        number != 0 ? --e.target.nextElementSibling.innerText: false;
+        disabledButton();
+    }
+    const onPlus = (e) => {
+        ++e.target.previousElementSibling.innerText;
+        disabledButton();
+    }
+
+    $('.adult-minus').on('click', onMinus);
+    $('.adult-plus').on('click', onPlus);
+    $('.children-minus').on('click', onMinus);
+    $('.children-plus').on('click', onPlus);
+    $('.baby-minus').on('click', onMinus);
+    $('.baby-plus').on('click', onPlus);
+
+    $('.bedroom-minus').on('click', onMinus);
+    $('.bedroom-plus').on('click', onPlus);
+    $('.bed-minus').on('click', onMinus);
+    $('.bed-plus').on('click', onPlus);
+    $('.bathroom-minus').on('click', onMinus);
+    $('.bathroom-plus').on('click', onPlus);
+
+    disabledButton();
+
+    const onClear = (dropdownMenu) => {
+        const quantity = dropdownMenu.querySelectorAll('.quantity');
+        quantity.forEach((el) => {
+            el.innerText = 0;
+        });
+        disabledButton();
+    }
+
+    const onSetResult = () => {
+
+        for (let i = 0; i < guestsForm.length; i++) {
+            let quantityItems = guestsForm[i].querySelectorAll('.quantity');
+            let finalQuantity = 0;
+            for (let i = 0; i < quantityItems.length; i++) {
+                finalQuantity += +quantityItems[i].innerText;
+            }
+            const inputGuest = guestsForm[i].querySelector('input');
+    
+            switch(finalQuantity) {
+                case 0:
+                    inputGuest.setAttribute('placeholder', 'Сколько гостей');
+                    break;
+                case 1:
+                    inputGuest.setAttribute('placeholder', `${ finalQuantity} гость`);
+                    break;
+                case 2:
+                    inputGuest.setAttribute('placeholder', `${ finalQuantity} гостя`);
+                    break;
+                default:
+                    inputGuest.setAttribute('placeholder', `${ finalQuantity} гостей`);
+                    break;
+              }
+        }
+    }
+
+    for (let i = 0; i < clearBtn.length; i++) {
+        let dropdownMenu = $('.form-elem__dropdown-guests')[i];
+        clearBtn[i].addEventListener('click', () => onClear(dropdownMenu));
+        setResultBtn[i].addEventListener('click', () => onSetResult(dropdownMenu));
+    }
+    const guestsForm = $('.form-elem__dropdown-wrapper-guest');
+    onSetResult();
+}
