3e165bc1b6d78608c33cfd3b4810d930ca96e0f2
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2021-01-11 19:17:51 +0500

    refactor: webpack config

---
+++ src/pug/pages/ui_kit/headers_and_footers/headers_and_footers.js
@@ -0,0 +1,5 @@
+import '../../../components/colored_logo.scss';
+// import './headers_and_footers.scss';
+import '../../../components/buttons/buttons.scss';
+import '../../../components/header.scss';
+import '../../../components/footer.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 let path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist'),
+  assets: path.join(__dirname, './src/assets'),
+}
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
 
 module.exports = {
   mode: 'development',
   entry: {
-    cards: path.resolve(__dirname, './src/pug/pages/ui_kit/cards/card.js')
+    cards: `${PAGES_DIR}ui_kit/cards/card.js`,
+    headers_and_footers: `${PAGES_DIR}ui_kit/headers_and_footers/headers_and_footers.js`,
   } ,
   output: {
     path: path.resolve(__dirname, 'dist')
@@ -29,6 +37,17 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
       }
     ]
   },
@@ -43,39 +62,43 @@ module.exports = {
     //   cleanStaleWebpackAssets: false,
     //   cleanOnceBeforeBuildPatterns: ['**/*', '!files*', '!files/*.jpg', '!files/*.mp3'],
     // }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    
     new HtmlWebpackPlugin({
         filename: 'color_type.html',
-        template: path.resolve(__dirname, './src/pug/pages/ui_kit/color_type/color_type.html'),
+        template: `${PAGES_DIR}ui_kit/color_type/color_type.pug`,
         chunks: ['color_type']
       }),
 
     new HtmlWebpackPlugin({
     filename: 'form_elem.html',
-    template: path.resolve(__dirname, './src/pug/pages/ui_kit/form_elem/form_elem.html'),
+    template: `${PAGES_DIR}ui_kit/form_elem/form_elem.pug`,
     chunks: ['form_elem']
     }),
 
     new HtmlWebpackPlugin({
     filename: 'cards.html',
-    template: path.resolve(__dirname, './src/pug/pages/ui_kit/cards/cards.html'),
+    template: `${PAGES_DIR}ui_kit/cards/cards.pug`,
     chunks: ['cards']
-    }),
-
+    }),    
     new HtmlWebpackPlugin({
       filename: 'headers_and_footers.html',
-      template: path.resolve(__dirname, './src/pug/pages/ui_kit/headers_and_footers/headers_and_footers.html'),
-      chunks: ['headers_and_footers']
+      template: `${PAGES_DIR}ui_kit/headers_and_footers/headers_and_footers.pug`,
+      chunks: ['headers_and_footers'],
+      inject: true
     }),
     new HtmlWebpackPlugin({
       filename: 'landing_page.html',
-      template: path.resolve(__dirname, './src/pug/pages/website/landing_page/landing_page.html'),
+      template: `${PAGES_DIR}website/landing_page/landing_page.pug`,
       chunks: ['landing_page']
     }),
 
-    // new CopyPlugin([
-    //   { from: '../../../../assets', to: 'src/assets' },
-    //   // { from: 'src/icons', to: 'icons' },
-    //   // { from: 'src/assets/', to: '' },
-    // ]),
+    new CopyPlugin({
+      patterns:  [
+        { from: `${PATHS.assets}/fonts/Montserrat`, to: `${PATHS.dist}/fonts` }
+      ]
+    }),
   ]
 };
