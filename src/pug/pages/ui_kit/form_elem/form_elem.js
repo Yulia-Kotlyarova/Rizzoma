da583d7f25210e42c23bf89616b68bf443bba8e2
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2021-01-13 12:54:30 +0500

    remove: useless dist folders

--- src/pug/pages/ui_kit/form_elem/form_elem.js
+++ src/pug/pages/ui_kit/form_elem/form_elem.js
@@ -1,13 +1,33 @@
 import '../../../../jquery/simplePagination.css';
-import '../../../components/inputs/input.scss';
 import '../../../../../node_modules/air-datepicker/dist/css/datepicker.min.css';
 import '../../../components/datepicker/datepicker.scss';
-// import './form_elem.scss';
 
+import './form_elem.scss';
+import '../../../components/buttons/buttons.scss';
+import '../../../components/buttons/toggle.scss';
+import '../../../components/buttons/radio.scss';
+import '../../../components/buttons/pagination.scss';
+import '../../../components/buttons/like.scss';
+import '../../../components/buttons/rich.scss';
+
+import '../../../components/benefits/benefits.scss';
+import '../../../components/bullet/bullet.scss';
+import '../../../components/response/response.scss';
+import '../../../components/avatar/avatar.scss';
+
+import '../../../components/rate/rate.scss';
+import '../../../components/range/range.scss';
+import '../../../components/form_elem/actions.scss';
+
+import '../../../components/inputs/input.scss';
+import '../../../components/inputs/dropdown.scss';
+import '../../../components/inputs/date_drop.scss';
+import '../../../components/inputs/checkbox.scss';
+
+
+import '../../../components/range/range.js';
 import '../../../components/inputs/date_drop_numb.js';
 import '../../../components/inputs/drop_down_room.js';
 import '../../../components/inputs/drop_down_guests.js';
-
-import '../../../pages/ui_kit/form_elem/range.js';
 // import '../../../../../node_modules/air-datepicker/dist/js/datepicker.min.js';
 // import '../../../components/datepicker/datepicker.js';
