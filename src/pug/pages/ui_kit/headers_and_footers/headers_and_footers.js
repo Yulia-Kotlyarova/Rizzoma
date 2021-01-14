a65e24e755c0dabdd61555bb5a31aa40bea7cde6
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2021-01-14 20:56:23 +0500

    refactor: footer and header page pixel perfect

--- src/pug/pages/ui_kit/headers_and_footers/headers_and_footers.js
+++ src/pug/pages/ui_kit/headers_and_footers/headers_and_footers.js
@@ -2,4 +2,5 @@ import '../../../components/colored_logo.scss';
 import '../../../components/buttons/buttons.scss';
 import '../../../components/header.scss';
 import '../../../components/footer.scss';
-// import './headers_and_footers.scss';
+import './headers_and_footers.scss';
+import '../../../components/inputs/input.scss';
